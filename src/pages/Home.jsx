8a4e6cc492cff17dacbd5a7a9c18662a301311bb
ayoub3bidi <mrayoubabidi@gmail.com>
2022-03-13 09:57:23 +0100

    Creating Announcement component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div className="container">
+    <div>
+        <Announcement/>
         <Navbar />
     </div>
   )
